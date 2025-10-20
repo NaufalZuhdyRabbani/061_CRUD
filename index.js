e0b45f81e87dc03fcdd8b522135f8cca651b941b
NaufalZuhdyRabbani <naufalkzrz@gmail.com>
2025-10-20 10:21:38 +0700

    kelima commit

--- index.js
+++ index.js
@@ -28,27 +28,51 @@ db.connect((err) => {
     }
 });
 
-//buat Method GET DAN POST
-app.get('/api/users', (req, res) => {
-    db.query('SELECT * FROM mahasiswa', (err, results) => {
-        if (err) {
-            console.error('Error fetching users: ' + err.stack);
-            res.status(500).send('Error fetching users');
-            return;
+//POST
+app.post('/api/users', (req, res) => {
+    const { nama, nim, kelas, } = req.body;
+    if (!nama || !nim || !kelas) {
+        return res.status(400).json({ message: 'Nama, NIM, dan Kelas harus diisi' });
+    }
+
+    db.query(
+        'INSERT INTO mahasiswa (nama, nim, kelas) VALUES (?, ?, ?)', [nama, nim, kelas],
+        (err, results) => {
+            if (err) {
+                console.error('Gagal menambahkan data' + err.stack);
+                return res.status(500).json({ message: 'Gagal menambahkan data' });
+            }
+            res.status(201).json({ message: 'Data berhasil ditambahkan' });
         }
-        res.json(results);
-    })
+    );
 });
 
-app.post('/api/users', (req, res) => {
+app.put('/api/users/:id', (req, res) => {
+    const userId = req.params.id;
     const { nama, nim, kelas } = req.body;
-    db.query('INSERT INTO mahasiswa (nama, nim, kelas) VALUES (?, ?, ?)', [nama, nim, kelas], (err, results) => {
-        if (err) {
-            console.error('Error inserting user: ' + err.stack);
-            res.status(500).send('Error inserting user');
-            return;
+    db.query(
+        'UPDATE mahasiswa SET nama = ?, nim = ?, kelas = ? WHERE id = ?', [nama, nim, kelas, userId],
+        (err, results) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ message: 'Database Error' });
+            }
+            res.json({ message: 'User berhasil diupdate' });
         }
-        res.status(201).json({ message: 'User added successfully', id: results.insertId });
-    });
+    );
 });
+
+app.delete('/api/users/:id', (req, res) => {
+    const userId = req.params.id;
+    db.query(
+        'DELETE FROM mahasiswa WHERE id = ?', [userId],
+        (err, results) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ message: 'Database Error' });
+            }
+            res.json({ message: 'User berhasil dihapus' });
+        });
+});
+
 
